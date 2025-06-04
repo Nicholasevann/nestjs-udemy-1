fe4d4c5f8c02d3c2518547db320c750452304aa3
Nicholasevann <nnicholasevan@gmail.com>
2025-06-04 17:21:05 +0800

    course dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,17 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Get,
+  Headers,
+  Ip,
   Param,
   ParseIntPipe,
   Post,
   Query,
+  ValidationPipe,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -16,20 +21,19 @@ export class UsersController {
   }
 
   @Post()
-  public createUser(@Body() body: any) {
-    console.log('User data:', body);
+  public createUser(@Body() createUserDto: CreateUserDto) {
+    console.log('User data:', createUserDto);
     return 'User created';
   }
 
   @Get('/:id/{:optional}')
   public getUsersById(
-    @Param('id') id: any,
+    @Param('id', ParseIntPipe) id: number,
     @Param('optional') optional?: any,
-    @Query('limit') limit?: any,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit?: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page?: number,
   ) {
-    console.log(typeof id);
-    console.log(typeof limit);
-    console.log(optional);
+    console.log(limit, page);
     if (optional) {
       return `ID is ${id} and optional parameter is ${optional}`;
     } else {
